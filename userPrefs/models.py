ef0d106ea88d8664be3fcf67e257af47c06efc40
Luis Obando <110127391+FrostLord98@users.noreply.github.com>
2024-09-18 23:59:37 -0400

    Delete userPrefs directory

--- userPrefs/models.py
+++
@@ -1,9 +0,0 @@
-from biblioz import db
-from sqlalchemy import ForeignKey
-
-class UserFavoriteGenre(db.Model):
-
-    __tablename__ = 'user_favorite_genres'
-
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    genre_id = db.Column(db.Integer, db.ForeignKey('genre.id'), primary_key=True)
--- userPrefs/schemas.py
+++
@@ -1,4 +0,0 @@
-from marshmallow import Schema, fields
-
-class UserFavoriteGenreSchema(Schema):
-    genre_ids = fields.List(fields.Integer)
