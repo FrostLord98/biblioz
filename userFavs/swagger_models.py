949bfdb6f08326463fe89aeb2efd2b2556c9e263
Luis Obando <110127391+FrostLord98@users.noreply.github.com>
2024-09-18 23:59:31 -0400

    Delete userFavs directory

--- userFavs/swagger_models.py
+++
@@ -1,7 +0,0 @@
-from flask_restx import fields, Namespace
-
-api = Namespace('userFavorites', description='Libros favoritos del usuario')
-
-user_favorites_model = api.model('UserFavoriteBook', {
-    'book_ids': fields.List(fields.Integer)
-})
